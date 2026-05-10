85b4caaad3a58786e8e73dfdf506aab6f619f57a
neocode24 <neocode24@gmail.com>
2026-05-11 08:41:25 +0900

    Update veil.rb

--- Casks/veil.rb
+++ Casks/veil.rb
@@ -1,6 +1,6 @@
 cask "veil" do
   version "0.3.0"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "sha256:316b4d4148edb2c24d9a8c3cc5060dad0aed5b99ab44e570bb67ad22aa133932"
 
   url "https://github.com/neocode24/Veil/releases/download/v#{version}/Veil-v#{version}.zip"
   name "Veil"
